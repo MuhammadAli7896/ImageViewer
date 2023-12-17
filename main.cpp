c61c95a01ac002fecafc2494e0f71e60af2ca8fb
smmkDev <135430890+smmkDev@users.noreply.github.com>
2023-12-17 19:44:16 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 #include <vector>
-#include <experimental/filesystem>
+#include <filesystem>
 
-namespace fs = std::experimental::filesystem;
+namespace fs = std::filesystem;
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
@@ -26,7 +25,7 @@ void LoadImagesFromFolder(const std::string& folderPath) {
 		if (fs::is_regular_file(entry)) {
 			// Open the file to check if it's a valid image using SDL_image
 			SDL_RWops* rwops = SDL_RWFromFile(entry.path().string().c_str(), "rb");
-			if (rwops != nullptr) { 
+			if (rwops != nullptr) {
 				// loading all types of extensions for an image
 				if (IMG_isPNG(rwops) || IMG_isJPG(rwops) || IMG_isBMP(rwops) ||
 					IMG_isGIF(rwops) || IMG_isICO(rwops) || IMG_isLBM(rwops) ||
@@ -44,27 +43,46 @@ void LoadImagesFromFolder(const std::string& folderPath) {
 }
 
 
-bool LoadCurrentImage() {
+bool LoadCurrentImage(SDL_Surface *window_surface) {
 if (currentImageIndex >= 0 && currentImageIndex < static_cast<int>(imagePaths.size())) {
 		SDL_Surface* surface = IMG_Load(imagePaths[currentImageIndex].c_str());
 		if (surface != nullptr) {
-			SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-			SDL_FreeSurface(surface);
-			if (texture != nullptr) {
-				// Clear previous textures
-				SDL_RenderClear(renderer);
-				SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-				SDL_RenderPresent(renderer);
-				SDL_DestroyTexture(texture);
-				return true;
-			}
+            SDL_BlitSurface(surface, NULL, window_surface, NULL);
+            SDL_UpdateWindowSurface(window);
+//			SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+//			SDL_FreeSurface(surface);
+//			if (texture != nullptr) {
+//				// Clear previous textures
+//				SDL_RenderClear(renderer);
+//				SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+//				SDL_RenderPresent(renderer);
+//				SDL_DestroyTexture(texture);
+//				return true;
+//			}
 		}
 	}
 	return false;
 }
 
+void on_click(SDL_MouseButtonEvent &button, SDL_Surface *window_surface)
+{
+    if(button.button == SDL_BUTTON_LEFT)
+    {
+        std::cout<< button.x << button.y << std::endl;
+        if (button.x >= 0 && button.x<= 50 && button.y >= 300 && button.y <= 350) {
+            currentImageIndex = (currentImageIndex - 1 + imagePaths.size()) % imagePaths.size();
+            LoadCurrentImage(window_surface);
+        }
+        else if (button.x >= windowWidth - 50 && button.x <= windowWidth && button.y >= 300 && button.y <= 350) {
+            currentImageIndex = (currentImageIndex + 1) % imagePaths.size();
+            LoadCurrentImage(window_surface);
+       }
+    }
+}
+
 int main(int argc, char* argv[]) {
 	// Initialize SDL
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL initialization failed: %s", SDL_GetError());
 		return -1;
@@ -78,22 +96,33 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	// Create a renderer
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Renderer creation failed: %s", SDL_GetError());
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return -1;
-	}
+	SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    SDL_Surface *arrow_left = IMG_Load("arrow_left.jpg");
+	SDL_Surface *arrow_right = IMG_Load("arrow_right.jpg");
+	SDL_Rect rect;
+	rect.x = 1; rect.y = 300;
+	SDL_BlitSurface(arrow_left, NULL, window_surface, &rect);
+	rect.x = 750;
+	SDL_BlitSurface(arrow_right, NULL, window_surface, &rect);
+	SDL_UpdateWindowSurface(window);
+
+
+//	// Create a renderer
+//	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+//	if (renderer == nullptr) {
+//		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Renderer creation failed: %s", SDL_GetError());
+//		SDL_DestroyWindow(window);
+//		SDL_Quit();
+//		return -1;
+//	}
 
 	// Load images from a folder
-	std::string imageFolder = "C:\\Users\\Dev\\source\\repos\\Classic-Fumigation\\src\\images"; // path for the folder from where the images will be extracted
+	std::string imageFolder = "C:\\Users\\fahad\\Pictures\\Saved Pictures"; // path for the folder from where the images will be extracted
 	LoadImagesFromFolder(imageFolder);
 
 	// Load the first image
 	if (!imagePaths.empty()) {
-		LoadCurrentImage();
+		LoadCurrentImage(window_surface);
 	}
 
 	// Main loop
@@ -109,30 +138,20 @@ int main(int argc, char* argv[]) {
 				switch (e.key.keysym.sym) {
 				case SDLK_LEFT:
 					currentImageIndex = (currentImageIndex - 1 + imagePaths.size()) % imagePaths.size();
-					LoadCurrentImage();
+					LoadCurrentImage(window_surface);
 					break;
 				case SDLK_RIGHT:
 					currentImageIndex = (currentImageIndex + 1) % imagePaths.size();
-					LoadCurrentImage();
+					LoadCurrentImage(window_surface);
 					break;
 				default:
 					break;
 				}
 			}
 			else if (e.type == SDL_MOUSEBUTTONDOWN) {
-				int mouseX, mouseY;
-				SDL_GetMouseState(&mouseX, &mouseY);
-
+				on_click(e.button, window_surface);
 				// Check if the mouse click is within the next button area
-				if (mouseX >= windowWidth - 50 && mouseX <= windowWidth && mouseY >= 0 && mouseY <= 50) {
-					currentImageIndex = (currentImageIndex + 1) % imagePaths.size();
-					LoadCurrentImage();
-				}
-				// Check if the mouse click is within the previous button area
-				else if (mouseX >= 0 && mouseX <= 50 && mouseY >= 0 && mouseY <= 50) {
-					currentImageIndex = (currentImageIndex - 1 + imagePaths.size()) % imagePaths.size();
-					LoadCurrentImage();
-				}
+
 			}
 		}
 	}

3a6075f9993d23578924cfef8e1c20b374040011
unknown <muhammadali30804@gmail.com>
2023-12-20 21:07:48 +0500

    update

--- main.cpp
+++ main.cpp
@@ -13,14 +13,23 @@ SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 std::vector<std::string> imagePaths;
 int currentImageIndex = 0;
-const int windowWidth = 800;  // Adjust the window width
-const int windowHeight = 600; // Adjust the window height
 const int size_of_arrows = 50;
-const int gap_in_x = 7;
+const int gap_in_x = 2;
 const int gap_in_y = 0;
-std::string imageFolder = "C:\\Users\\Dev\\source\\repos\\Classic-Fumigation\\src\\images"; // path for the folder from where the images will be extracted
+int windowHeight = 0;
+int windowWidth = 0;
+std::string imageFolder = "C:\\Users\\Dev\\source\\repos\\Classic-Fumigation\\src\\imaes"; // path for the folder from where the images will be extracted
 
 void LoadImagesFromFolder() {
+
+	// Check if the specified folder exists
+	if (!fs::exists(imageFolder) || !fs::is_directory(imageFolder)) {
+		// If not, use the project's directory as the default folder path
+		// Adjust the subfolder path based on your project's structure
+		imageFolder = fs::current_path().string() + "/images"; // Use "images" as the default subfolder
+		std::cout << "Specified folder not found. Using default folder path: " << imageFolder << std::endl;
+	}
+
 	// Initialize SDL_image
 	int imgFlags = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF;
 	if (!(IMG_Init(imgFlags) & imgFlags)) {
@@ -50,39 +59,120 @@ void LoadImagesFromFolder() {
 }
 
 
-bool LoadCurrentImage(SDL_Surface *window_surface) {
+//bool LoadCurrentImage(SDL_Surface *window_surface) {
+//
+//	if (currentImageIndex >= 0 && currentImageIndex < static_cast<int>(imagePaths.size())) {
+//		SDL_Surface* surface = IMG_Load(imagePaths[currentImageIndex].c_str());
+//		SDL_Rect rect;
+//		rect.x = rect.y = 0;
+//        rect.w = windowWidth; rect.h = windowHeight;
+//		if (surface != nullptr) {
+//            SDL_BlitScaled(surface, NULL, window_surface, &rect);
+////          SDL_BlitSurface(surface, NULL, window_surface, &rect);
+//            SDL_UpdateWindowSurface(window);
+////			SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+////			SDL_FreeSurface(surface);
+////			if (texture != nullptr) {
+////				// Clear previous textures
+////				SDL_RenderClear(renderer);
+////				SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+////				SDL_RenderPresent(renderer);
+////				SDL_DestroyTexture(texture);
+////				return true;
+////			}
+//		}
+//	}
+//    SDL_Surface *arrow_left = IMG_Load("arrow_left.jpg");
+//	SDL_Surface *arrow_right = IMG_Load("arrow_right.jpg");
+//	SDL_Rect rect;
+//	rect.x = gap_in_x; rect.y = windowHeight / 2 - (size_of_arrows / 2);
+//	SDL_BlitSurface(arrow_left, NULL, window_surface, &rect);
+//	rect.x = windowWidth - size_of_arrows - gap_in_x;
+//	SDL_BlitSurface(arrow_right, NULL, window_surface, &rect);
+//	SDL_UpdateWindowSurface(window);
+//	return false;
+//}
 
-if (currentImageIndex >= 0 && currentImageIndex < static_cast<int>(imagePaths.size())) {
+bool LoadCurrentImage(SDL_Surface* window_surface) {
+	if (currentImageIndex >= 0 && currentImageIndex < static_cast<int>(imagePaths.size())) {
 		SDL_Surface* surface = IMG_Load(imagePaths[currentImageIndex].c_str());
 		SDL_Rect rect;
-		rect.x = rect.y = 0;
-        rect.w = windowWidth; rect.h = windowHeight;
+
+		// Calculate the destination rectangle position and dimensions
+		int imageWidth = surface->w;
+		int imageHeight = surface->h;
+		float min_size = 0.5;
+		float increase_size = 2.5;
+
+		// Check if the image dimensions are greater than the screen size - 30
+		if (imageWidth >= (windowWidth - 30) || imageHeight >= (windowHeight - 30)) {
+			// Decrease image dimensions
+			float aspectRatio = static_cast<float>(imageWidth) / static_cast<float>(imageHeight);
+
+			if (aspectRatio >= 1.0f) {
+				imageWidth = windowWidth - 120;
+				//imageHeight = static_cast<int>(imageWidth / aspectRatio);
+				imageHeight = windowHeight - 120;
+			}
+			else {
+				imageHeight = windowHeight;
+				imageWidth = static_cast<int>(imageHeight * aspectRatio);
+			}
+		}
+		else if (imageWidth <= (windowWidth * min_size) || imageHeight <= (windowHeight * min_size))
+		{ // if the image is so small then increment its dimensions
+			imageHeight *= increase_size;
+			imageWidth *= increase_size;
+		}
+
+		int posX = (windowWidth - imageWidth) / 2;
+		int posY = (windowHeight - imageHeight) / 2;
+
+		rect.x = posX;
+		rect.y = posY;
+		rect.w = imageWidth;
+		rect.h = imageHeight;
+
+		if (imageHeight >= windowHeight - 40)
+		{
+			imageHeight = windowHeight - 60;
+		}
+		if (imageWidth >= windowWidth - 40)
+		{
+			imageWidth = windowWidth - 60;
+		}
+
 		if (surface != nullptr) {
-            SDL_BlitScaled(surface, NULL, window_surface, &rect);
-//          SDL_BlitSurface(surface, NULL, window_surface, &rect);
-            SDL_UpdateWindowSurface(window);
-//			SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-//			SDL_FreeSurface(surface);
-//			if (texture != nullptr) {
-//				// Clear previous textures
-//				SDL_RenderClear(renderer);
-//				SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-//				SDL_RenderPresent(renderer);
-//				SDL_DestroyTexture(texture);
-//				return true;
-//			}
+			// Clear the window
+			SDL_FillRect(window_surface, nullptr, SDL_MapRGB(window_surface->format, 0, 0, 0));
+
+			// Blit the scaled image onto the window_surface
+			SDL_BlitScaled(surface, nullptr, window_surface, &rect);
+
+			// Blit the arrows onto the window_surface
+			SDL_Surface* arrow_left = IMG_Load("arrow_left.jpg");
+			SDL_Surface* arrow_right = IMG_Load("arrow_right.jpg");
+			rect.x = gap_in_x;
+			rect.y = windowHeight / 2 - (size_of_arrows / 2);
+			SDL_BlitSurface(arrow_left, nullptr, window_surface, &rect);
+			rect.x = windowWidth - size_of_arrows - gap_in_x;
+			SDL_BlitSurface(arrow_right, nullptr, window_surface, &rect);
+
+			// Update the window surface
+			SDL_UpdateWindowSurface(window);
+
+			SDL_FreeSurface(arrow_left);
+			SDL_FreeSurface(arrow_right);
+			SDL_FreeSurface(surface);
+
+			return true;
 		}
 	}
-    SDL_Surface *arrow_left = IMG_Load("arrow_left.jpg");
-	SDL_Surface *arrow_right = IMG_Load("arrow_right.jpg");
-	SDL_Rect rect;
-	rect.x = gap_in_x; rect.y = windowHeight / 2 - (size_of_arrows / 2);
-	SDL_BlitSurface(arrow_left, NULL, window_surface, &rect);
-	rect.x = windowWidth - size_of_arrows - gap_in_x;
-	SDL_BlitSurface(arrow_right, NULL, window_surface, &rect);
-	SDL_UpdateWindowSurface(window);
+
 	return false;
 }
+
+
 
 void on_click(SDL_MouseButtonEvent &button, SDL_Surface *window_surface)
 {
@@ -107,13 +197,35 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	// Get the screen size
+	SDL_DisplayMode dm;
+	if (SDL_GetCurrentDisplayMode(0, &dm) != 0) {
+		std::cerr << "SDL_GetCurrentDisplayMode failed: " << SDL_GetError() << std::endl;
+		IMG_Quit();
+		SDL_Quit();
+		return -1;
+	}
+
+	windowWidth = dm.w;
+	windowHeight = dm.h - 60;
+
+
 	// Create a window
 	window = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+	if (window == nullptr) {
+		std::cerr << "Window creation failed: " << SDL_GetError() << std::endl;
+		IMG_Quit();
+		SDL_Quit();
+		return -1;
+	}
+
+	// Create a window
+	/*window = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
 	if (window == nullptr) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Window creation failed: %s", SDL_GetError());
 		SDL_Quit();
 		return -1;
-	}
+	}*/
 
 
 //	// Create a renderer
